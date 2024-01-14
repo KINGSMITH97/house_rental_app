be41a87a3b457aac4888fb693a41e2a107423e71
King Frigg <kingsleyappiah2143@gmail.com>
2024-01-14 01:35:11 +0000

    setup

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors(_);
+
+  Color kGray200 = const Color(0xffcccccc);
+  Color kTeal300 = const Color(0xff59d4de);
+  Color kYellow = const Color(0xfff0c659);
+  Color kPrimary = const Color(0xff2f2f2f);
+  Color kTextColor = const Color(0xff585858);
+  Color kWhite = const Color(0xffffffff);
+}
